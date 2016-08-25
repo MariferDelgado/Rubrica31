5745f73990214708483395441cd7c5519c6ac56f
MariferDelgado <fer1995am@gmail.com>
2016-08-25 15:47:24 -0500

    Hola Mundo

--- src/proyectoingenieria/ProyectoIngenieria.java
+++ src/proyectoingenieria/ProyectoIngenieria.java
@@ -5,6 +5,8 @@
  */
 package proyectoingenieria;
 
+import java.io.IOException;
+
 /**
  *
  * @author Juninho
@@ -14,12 +16,21 @@ public class ProyectoIngenieria {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Universidad Laica Eloy Alfaro de Manabí");
-        System.out.println("Hola Ing soy Mafer");
-        
-        
-         
+  public static void main(String[] args)throws IOException{
+      
+        // TODO code application logic here      
+         Menu();
     }
-}
+public static void Menu()throws IOException{
+     System.out.println("\n\n\t\t\t=============Menú=================");
+     System.out.println("\t\t\t=                                    =");
+     System.out.println("\t\t\t= 1-Universidad Laica Eloy Alfaro de Manabí =");
+     System.out.println("\t\t\t= 2- Hola Ing soy Mafer                     =");
+     System.out.println("\t\t\t= 3- Salir                           =");
+     System.out.println("\t\t\t======================================");
+     System.out.println("\t\t\tOpcion: ");
+    
+    } 
+        
+    }
+
